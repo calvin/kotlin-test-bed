a8fd4d438680755c62f925d9013f540daaaf2c4d
Seyeong Jeong <seyeong@ty.pe.kr>
2019-06-02 17:23:32 +1000

    Initial commit

---
+++ src/main/kotlin/testbed/Calculator.kt
@@ -0,0 +1,7 @@
+package testbed
+
+class Calculator(private val a: Int, private val b: Int) {
+    fun sum(): Int {
+        return this.a + this.b
+    }
+}
---
+++ src/test/kotlin/testbed/CalculatorSpec.kt
@@ -0,0 +1,17 @@
+package testbed
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+
+class CalculatorSpec: StringSpec({
+    "this should be ok"  {
+        println("hello hello")
+        "s" shouldBe "s"
+    }
+
+    "this should be also ok" {
+        val c = Calculator(1, 2)
+        c.sum() shouldBe 3
+    }
+})
